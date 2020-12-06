d22b6936f5887343ebdaae94f9d979a747af8257
Kacper Grzybkowski <kacperg@gmail.com>
2020-12-06 21:12:29 +0100

    #1 progrma konertujacy C na F

---
+++ App.java
@@ -0,0 +1,20 @@
+class App {
+	public static void main(String args[]) {
+
+	// ZADANIE 1
+	
+	double tc, tf;
+	
+	tf = 23.31;
+	
+	tc = (tf - 32)*5/9;
+	
+	System.out.println("TF= :" +tf);
+	System.out.println("TC= :" +tc);
+	
+	
+	
+	}
+
+
+}
